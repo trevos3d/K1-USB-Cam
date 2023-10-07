9e751e20e3cc907e531437745c75851e5e2bdcc3
Rafael Ferreira <rafael@trevos3d.com>
2023-10-07 07:10:56 -0300

    Fix mjpeg_streamer call

--- script/startup_usb_camera.sh
+++ script/startup_usb_camera.sh
@@ -5,4 +5,4 @@ kill -9 $(fuser /dev/video4)
 kill -9 $(fuser /dev/video5)
 kill -9 $(fuser /dev/video6)
 
-mjpg_streamer -b -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video4" -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video4" -o "/usr/lib/mjpg-streamer/output_http.so -p 8080"
+mjpg_streamer -b -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video4" -o "/usr/lib/mjpg-streamer/output_http.so -p 8080"
