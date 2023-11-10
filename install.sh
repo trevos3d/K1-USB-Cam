9aa2a2c7e902dd534265d0b475900a42f2ff527a
Rafael Ferreira <rafael@trevos3d.com>
2023-11-10 20:58:56 -0300

    Change from bash to sh shell

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/bin/sh
 
 Single_USB_Camera()
 {
-    echo -e "\n$msg \nInstalling Single USB camera...\n"
+    echo "\n$msg \nInstalling Single USB camera...\n"
     cp -f script/single_cam.sh /usr/data/startup_usb_camera.sh
     chmod +x /usr/data/startup_usb_camera.sh
 
@@ -15,12 +15,12 @@ Single_USB_Camera()
     chmod +x /etc/init.d/S99usb_camera
     /etc/init.d/S99usb_camera start
 
-    echo -e "\n \e[1;31m Please consider reboot your printer to make sure that all process will work property.\e[0m \n" 
+    echo  "\n \e[1;31m Please consider reboot your printer to make sure that all process will work property.\e[0m \n" 
 }
 
 Dual_USB_Camera()
 {
-    echo -e "\n$msg \nInstalling Dual USB camera...\n"
+    echo "\n$msg \nInstalling Dual USB camera...\n"
     cp -f script/dual_cam.sh /usr/data/startup_usb_camera.sh
     chmod +x /usr/data/startup_usb_camera.sh
 
@@ -33,39 +33,40 @@ Dual_USB_Camera()
     chmod +x /etc/init.d/S99usb_camera
     /etc/init.d/S99usb_camera start
 
-    echo -e "\n \e[1;31m Please consider reboot your printer to make sure that all process will work property.\e[0m \n" 
+    echo "\n \e[1;31m Please consider reboot your printer to make sure that all process will work property.\e[0m \n" 
 }
 
 
 echo "" 
-PS="How can I help you today? "
+PS=
 
-select msg in "Install Single USB Camera" "Install K1 + USB Cam" Quit
+while : 
 do
+
+    
+
+    echo "1) Install Single USB Camera"
+
+    echo "2) Install K1 + USB Cam"
+
+    echo "3) exit"
+
+    echo "\nHow can I help you today? "
+
+    read msg;
+
     case $msg in
-        "Install Single USB Camera")
-            Single_USB_Camera ;;
-        "Install K1 + USB Cam")
-           Dual_USB_Camera ;;
-        "Quit")
+        1) echo "Install Single USB Camera"
+            Single_USB_Camera
+            exit ;;
+        2) echo "Install K1 + USB Cam"
+           Dual_USB_Camera 
+           exit ;;
+        3) echo "Quit"
            echo "We're done here."
-           break;;
+           exit ;;
         *)
-           echo "Ooops";;
+           echo "\nOoops, invalid option.\n"
     esac
+    sleep .5
 done
-
-
-
-
-#cp script/startup_usb_camera.sh /usr/data/startup_usb_camera.sh
-#chmod +x /usr/data/startup_usb_camera.sh
-
-#cp service/S99usb_camera /etc/init.d/S99usb_camera
-#chmod +x /etc/init.d/S99usb_camera
-
-#echo "Successfully Installed K1-USB-Cam"
-
-#echo "Starting Webcam Service"
-#chmod +x /etc/init.d/S99usb_camera
-#/etc/init.d/S99usb_camera start
