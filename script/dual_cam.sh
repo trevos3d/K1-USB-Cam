4d99f2eba7dab7c8dc0bbc0f12a90c9c62d72c6b
trevos3d <147241448+trevos3d@users.noreply.github.com>
2024-11-02 17:55:25 -0300

    code impruvements

--- script/dual_cam.sh
+++ script/dual_cam.sh
@@ -1,10 +1,12 @@
-#!/bin/sh
-sleep 120
+#!/bin/bash
+sleep 30
 
 kill -9 $(pidof mjpg_streamer)
-kill -9 $(fuser /dev/video4)
-kill -9 $(fuser /dev/video5)
-kill -9 $(fuser /dev/video6)
+for i in $(seq 4 8); do fuser /dev/video$i; done
+
+CAM1=$(v4l2-ctl --list-devices | grep -A 1 "CREALITY" | tail -n 1 | awk '{print $1}')
+CAM2=$(v4l2-ctl --list-devices | grep -v CREALITY | grep usb -A 1 | grep -E "/dev/video[0-9]+")
 
 
-mjpg_streamer -b -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video4" -i "/usr/lib/mjpg-streamer/input_uvc.so -d /dev/video5" -o "/usr/lib/mjpg-streamer/output_http.so -p 8080"
+
+mjpg_streamer -b -i "/usr/lib/mjpg-streamer/input_uvc.so -d $CAM1" -i "/usr/lib/mjpg-streamer/input_uvc.so -d $CAM2" -o "/usr/lib/mjpg-streamer/output_http.so -p 8080"
--- uninstall.sh
+++ uninstall.sh
@@ -5,4 +5,4 @@ echo "Removing all K1-USB-CAM files"
 
 rm /usr/data/startup_usb_camera.sh /etc/init.d/S99usb_camera
 
-echo "Uninstalled, consider reboot your printer."
+echo "Uninstalled, please reboot your printer."
