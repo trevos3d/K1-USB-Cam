eb8ed783a2aaf4452e4e09b50a7ecbb337661917
trevos3d <147241448+trevos3d@users.noreply.github.com>
2024-11-02 19:28:54 -0300

    fix blank spaces on variables

--- script/dual_cam.sh
+++ script/dual_cam.sh
@@ -4,8 +4,8 @@ sleep 30
 kill -9 $(pidof mjpg_streamer)
 for i in $(seq 4 8); do fuser /dev/video$i; done
 
-CAM1=$(v4l2-ctl --list-devices | grep -A 3 usb | grep -A 2 "CREALITY CAM" | tail -n 1 | tr -d ' ')
-CAM2=$(v4l2-ctl --list-devices | grep -v CREALITY | grep usb -A 1 | grep -E "/dev/video[0-9]+")
+CAM1=$(v4l2-ctl --list-devices | grep -A 3 usb | grep -A 2 "CREALITY CAM" | tail -n 1 | awk '{$1=$1; print}')
+CAM2=$(v4l2-ctl --list-devices | grep -v "CREALITY" | grep "usb" -A 1 | grep -E "/dev/video[0-9]+" | tail -n 1 | awk '{$1=$1; print}')
 export CAM1
 export CAM2
 
