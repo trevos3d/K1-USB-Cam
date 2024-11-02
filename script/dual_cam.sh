38ffcf592ffae80bcddd6523a283e2034852577d
trevos3d <147241448+trevos3d@users.noreply.github.com>
2024-11-02 18:13:22 -0300

    code impruvement

--- script/dual_cam.sh
+++ script/dual_cam.sh
@@ -6,7 +6,8 @@ for i in $(seq 4 8); do fuser /dev/video$i; done
 
 CAM1=$(v4l2-ctl --list-devices | grep -A 1 "CREALITY" | tail -n 1 | awk '{print $1}')
 CAM2=$(v4l2-ctl --list-devices | grep -v CREALITY | grep usb -A 1 | grep -E "/dev/video[0-9]+")
-
+export CAM1
+export CAM2
 
 
 mjpg_streamer -b -i "/usr/lib/mjpg-streamer/input_uvc.so -d $CAM1" -i "/usr/lib/mjpg-streamer/input_uvc.so -d $CAM2" -o "/usr/lib/mjpg-streamer/output_http.so -p 8080"
